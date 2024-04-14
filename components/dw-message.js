225b48ec7351712d09c5d5db1757d9fdaa1e8414
Diego-S-F <ds0382853@gmail.com>
2024-04-14 14:25:34 -0600

    add styles basic in first component dw-message

--- components/dw-message.js
+++ components/dw-message.js
@@ -1,10 +1,24 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 
 class DwMessage extends LitElement {
+    static styles = css`
+    :host {
+        display:  block;
+        border: 1px solid #334eff;
+        padding: 10px; 
+    }
+    div {
+        width: 100%;
+        text-align: center;
+    }
+    `
     render(){
         return  html `
             <div>
                 Hello from Lit
+            </div>
+            <div>
+                Second tag
             </div>
         `
     }
